c9fccdfa13413f7a9c2962a088961d8d87a9c652
tyler kim <yoonsoo.kim11@gmail.com>
2023-11-13 13:39:16 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+const multiply = num1 * num2
+
+let random = Math.floor(Math.random() * 10) + 1
+
+let num3 = 10
+let num4 = 6
+const mod = num3 % num4
+const max = Math.max(2, 3, 20)
